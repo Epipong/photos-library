efedc6e5ee090c44e143fef1ebc0334eb56ada1c
davy.tran <davytran1993@gmail.com>
2024-03-22 14:21:02 +0100

    Add copy feature

--- src/services/imagingEdgeSrvc.spec.ts
+++ src/services/imagingEdgeSrvc.spec.ts
@@ -1,14 +1,29 @@
 import { ImagingEdgeMobile } from "../entities/imagingEdgeMobile";
 import { iem } from "../fixtures/iem";
 import { ImagingEdgeSrvc } from "./imagingEdgeSrvc";
+import fs from "fs";
+import path from "path";
 
 describe("ImagingEdgeCtrl", () => {
-  describe("Import Files", () => {
+  beforeAll(() => {
+    const target = path.resolve(__dirname, "../test/target");
+    if (fs.existsSync(target)) {
+      fs.rmSync(target, { recursive: true });
+    }
+  });
+
+  describe("Import files by Extension", () => {
     it("should", () => {
       const manager = new ImagingEdgeSrvc(iem);
+      const target = path.resolve(__dirname, "../test/target");
 
       manager.importFilesByExt({ folder: "DCIM", ext: "JPG" });
-      expect(true).toBeTruthy();
+      const files = fs.readdirSync(target, { recursive: true });
+      expect(files.length).toBeGreaterThan(0);
     });
+  });
+
+  describe("Import all files", () => {
+    //
   });
 });
--- src/services/imagingEdgeSrvc.ts
+++ src/services/imagingEdgeSrvc.ts
@@ -1,31 +1,65 @@
 import { ImagingEdgeMobile } from "../entities/imagingEdgeMobile";
-import { logger } from "../infrastructures/logger";
 import fs from "fs";
 import path from "path";
+import cliProgress from "cli-progress";
+import { logger } from "../infrastructures/logger";
 
 type extension = "JPG" | "ARW" | "MP4";
 
 class ImagingEdgeSrvc {
   constructor(private iem: ImagingEdgeMobile) {}
 
+  private getYearMonthdayFormat(date: Date) {
+    const dateString = date.toISOString();
+    return dateString.substring(0, dateString.indexOf("T"));
+  }
+
+  private getFileName(file: string) {
+    return file.split("/").pop() || "";
+  }
+
+  private createFolder(folder: string): string {
+    const fullTargetPath = path.resolve(this.iem.targetPath, folder);
+    if (!fs.existsSync(fullTargetPath)) {
+      fs.mkdirSync(fullTargetPath, { recursive: true });
+    }
+    return fullTargetPath;
+  }
+
+  private copyFile(source: string, target: string, file: string) {
+    const targetFilePath = path.resolve(target, this.getFileName(file));
+    if (!fs.existsSync(targetFilePath)) {
+      fs.copyFileSync(source, targetFilePath);
+    }
+  }
+
   public importFilesByExt({ folder, ext }: { folder: string; ext: extension }) {
+    const dir = path.resolve(this.iem.sourcePath, folder);
     const files = fs
-      .readdirSync(path.resolve(this.iem.sourcePath, folder), {
+      .readdirSync(dir, {
         recursive: true,
       })
       .filter((file) => (file as string).endsWith(`.${ext}`));
-    logger.info(files);
+    const bar = new cliProgress.SingleBar({}, cliProgress.Presets.legacy);
+    bar.start(files.length, 0);
+    for (const file of files as string[]) {
+      const filePath = path.resolve(dir, file);
+      const { birthtime } = fs.statSync(filePath);
+      const dateFmt = this.getYearMonthdayFormat(birthtime);
+      const targetFolder = this.createFolder(`${dateFmt}/${ext}`);
+      this.copyFile(filePath, targetFolder, file);
+      bar.increment();
+    }
+    bar.stop();
   }
 
   public importFiles() {
     logger.info(
       `import files from '${this.iem.sourcePath}' to the target '${this.iem.targetPath}'.`,
     );
-    const files = fs.readdirSync(this.iem.sourcePath);
-
-    for (const file of files) {
-      logger.info(path.resolve(this.iem.sourcePath, file));
-    }
+    this.importFilesByExt({ folder: "DCIM", ext: "JPG" });
+    this.importFilesByExt({ folder: "DCIM", ext: "ARW" });
+    this.importFilesByExt({ folder: "PRIVATE/M4ROOT", ext: "ARW" });
   }
 }
 
