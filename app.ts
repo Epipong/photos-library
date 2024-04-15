3fa080ec377781e500621bb0c27dad88c52a4c78
davy.tran <davytran1993@gmail.com>
2024-04-15 09:44:58 +0200

    Add match pattern for the options

--- app.ts
+++ app.ts
@@ -6,6 +6,7 @@ import { PhotosProvider } from "./src/interfaces/photos.provider";
 import { AuthProvider } from "./src/interfaces/auth.provider";
 import { AuthFactory } from "./src/services/auth/auth-factory";
 import { PhotosFactory } from "./src/services/photos/photos-factory";
+import { match } from "ts-pattern";
 
 const main = async () => {
   const auth: AuthProvider = AuthFactory.createAuth(opt.options);
@@ -15,18 +16,14 @@ const main = async () => {
     ...opt.options,
     auth,
   });
-  const cmd: string = process.argv[2];
-  const commands: {
-    [cmd: string]: () => any;
-  } = {
-    import: () => manager.importFiles(opt.options.force as boolean),
-    export: () => manager.exportFiles(opt.options.force as boolean),
-    init: async () => await auth.init(),
-    token: async () => await auth.token(),
-    albums: async () => await photos.main(opt.options),
-    help: () => getopt.showHelp(),
-  };
-  commands[cmd in commands ? cmd : "help"]();
+  match(process.argv[2])
+    .with("import", () => manager.importFiles(opt.options.force as boolean))
+    .with("export", () => manager.exportFiles(opt.options.force as boolean))
+    .with("init", async () => await auth.init())
+    .with("token", async () => await auth.token())
+    .with("albums", async () => await photos.main(opt.options))
+    .with("help", () => getopt.showHelp())
+    .otherwise(() => getopt.showHelp())
 };
 
 main();
